42d22ae36044214b0fc47f976a32040fc092d1d5
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-08-22 22:32:13 +0930

    cheer

---
+++ function-2-1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+void hexDigits(int *numbers,int length)
+{
+    for(int i=0;i<length;i++)
+    {
+        cout << i << " " << *(numbers+i) << " ";
+        switch(*(numbers+i))
+        {
+            case 10:
+                cout << "A";
+                break;
+            case 11:
+                cout << "B";
+                break;
+            case 12:
+                cout << "C";
+                break;
+            case 13:
+                cout << "D";
+                break;
+            case 14:
+                cout << "E";
+                break;
+            case 15:
+                cout << "F";
+                break;
+            default:
+                cout << *(numbers+i);
+                break;
+        }
+        cout << endl;
+    }
+}
---
+++ main-2-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+extern void hexDigits(int *numbers,int length);
+
+int main()
+{
+    int *numbers;
+    int length;
+    cout << "What length would you like? ";
+    cin >> length;
+    numbers = new int[length];
+    int array[10] = {11,12,3,4,15,6,7,8,9,10};
+    for(int i=0; i<length; i++)
+    {
+        *(numbers+i) = array[i];
+        // cout << "Please enter the number you want to have in the " << i << "th element: ";
+        // cin >> *(numbers+i);
+    }
+    hexDigits(numbers, length);
+
+    return 0;
+}
